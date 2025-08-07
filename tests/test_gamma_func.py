939ce339405c5f27cb8153605ee8fe1f81f40517
Leon Maksin <leon@openai.com>
2025-08-07 10:03:28 -0700

    simplify

--- tests/test_gamma_func.py
+++ tests/test_gamma_func.py
@@ -1,16 +1,6 @@
 import sympy
 from math_verify import parser
 
-def add_math_mode_if_not_present(x: str) -> str:
-    x = x.strip()
-    if x.startswith("$"):
-        return x
-    if x.startswith("\\("):
-        return x
-    if x.startswith("\\["):
-        return x
-    return r"\(" + x + r"\)"
-
 def test_gamma_as_function():
     latex_str = r"\(\Gamma(\frac{\pi}{12})\)"
     [parsed_str] = parser.parse(latex_str, fallback_mode="no_fallback")
